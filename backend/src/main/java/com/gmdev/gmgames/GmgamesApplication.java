b9e4899c1b5f6735e7113c46b19c6ca425ce2682
Géssica Mendonça <gm.gessica@outlook.com>
2022-01-15 12:08:03 -0300

    project created

---
+++ backend/src/main/java/com/gmdev/gmgames/GmgamesApplication.java
@@ -0,0 +1,13 @@
+package com.gmdev.gmgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmgamesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gmdev/gmgames/GmgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmdev.gmgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
