90c7eb7cf9ffb11105e3bc21ea080c7feade8d43
Géssica Mendonça <gm.gessica@outlook.com>
2022-01-17 18:25:51 -0300

    Form layout

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,5 +1,43 @@
+import './styles.css';
+
 function Form() {
-    return (<h1>Página Form</h1>);
+
+        const game = {
+        id: 1,
+        image: "https://images.tcdn.com.br/img/img_prod/804441/jogo_shadow_of_the_tomb_raider_a_definitive_edition_ps4_2099_1_d3e497993d8a3688141d1438ecd6431d.jpg",
+        title: "Lara Croft",
+        count: 2,
+        score: 4.5
+    };
+
+    return (
+        <div className="gmgames-form-container">
+        <img className="gmgames-games-card-image" src={game.image} alt={game.title} />
+        <div className="gmgames-card-bottom-container">
+        <h3>"The Witcher"</h3>
+        <form className="gmgames-form">
+            <div className="form-group gmgames-form-group">
+                <label htmlFor="email">Informe seu email</label>
+                <input type="email" className="form-control" id="email" />
+            </div>
+            <div className="form-group gmgames-form-group">
+                <label htmlFor="score">Informe sua avaliação</label>
+                <select className="form-control" id="score">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                </select>
+            </div>
+            <div className="gmgames-form-btn-container">
+                <button type="submit" className="btn btn-primary gmgames-btn">Salvar</button>
+            </div>
+        </form >
+        <button className="btn btn-primary gmgames-btn mt-3">Cancelar</button>
+        </div >
+        </div >
+    );
 }
 
 export default Form;
